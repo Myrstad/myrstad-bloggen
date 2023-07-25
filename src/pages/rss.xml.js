857952489fa2a5fe7e9f72898d9ebd74e4b63f58
Myrstad <mortenmyrstad@gmail.com>
2023-07-25 15:26:20 +0200

    RSS endringer

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,21 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { formatBlogPosts } from "../js/utils";
 
 export async function get(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection("blog");
   return rss({
-    title: 'RSS | Myrstadbloggen',
-    description: 'Abonner her på Myrstadbloggen med RSS',
-    site: 'https://blog.mortenmyrstad.no/',
-    items: blog.map((post) => ({
+    title: "Myrstadbloggen",
+    description: "Abonner her på Myrstadbloggen med RSS",
+    customData: "<language>no_NO</language>",
+    site: "https://blog.mortenmyrstad.no/",
+    items: formatBlogPosts(blog).map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
       customData: post.data.customData,
       link: `/innlegg/${post.slug}/`,
+      categories: post.data.tags,
     })),
   });
 }
